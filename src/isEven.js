c653527b5de90c1c16a1ed4f3cc16be6d132efd7
xinyinxmm <xaviera1983nana@gmail.com>
2018-10-27 00:39:03 +1300

    o gong tweaked the code

--- src/isEven.js
+++ src/isEven.js
@@ -1,10 +1,12 @@
-/* eslint-disable no-confusing-arrow */
-const getTypeName = value => (value == null)
-  ? ''
-  : ` (${typeof value})`;
+/* eslint-disable no-confusing-arrow,implicit-arrow-linebreak */
+const getTypeName = value =>
+  value == null
+    ? ''
+    : ` (${typeof value})`;
 
-const isEven = n => (typeof n === 'number')
-  ? n % 2 === 0
-  : `expect number but received ${n}${getTypeName(n)}`;
+const isEven = n =>
+  typeof n === 'number'
+    ? n % 2 === 0
+    : `expect number but received ${n}${getTypeName(n)}`;
 
 exports.isEven = isEven;
