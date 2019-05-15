23e745c3166ac28f1b4f2a80471717eb4a202482
TitusLearn2Code <titus.yellow@gmail.com>
2019-05-15 20:02:57 +1200

    final solution

--- src/compute.js
+++ src/compute.js
@@ -1,8 +1,28 @@
 //  check value is not number return true
 //  if value is number return false
 
-function compute() {
-  return 'not implemented yet';
+function compute(a, b) {
+  if (typeof b !== 'number' && typeof a !== 'number') {
+    const s1 = 'expect: number, but received: ';
+    const and = ' and ';
+    const s2 = '"';
+    const s3 = ' (';
+    const s4 = ')';
+    return s1 + a + s3 + typeof a + s4 + and + s2 + b + s2 + s3 + typeof b + s4;
+  }
+  if (typeof a !== 'number') {
+    const s1 = 'expect: number, but received: ';
+    const s2 = ' (string)';
+    const s3 = '"';
+    return s1 + s3 + a + s3 + s2;
+  }
+  if (typeof b !== 'number') {
+    const s1 = 'expect: number, but received: ';
+    const s2 = ' (string)';
+    const s3 = '"';
+    return s1 + s3 + b + s3 + s2;
+  }
+  return a + b;
 }
 
 exports.compute = compute;
